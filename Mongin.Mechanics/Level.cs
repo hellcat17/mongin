a798603298425e3a7bfa41e6de666a55faaf9823
hellcat17 <dodgehellcat17@outlook.com>
2022-04-23 13:06:50 +0200

    add test for level boundaries

--- Mongin.Mechanics/Level.cs
+++ Mongin.Mechanics/Level.cs
@@ -2,7 +2,7 @@
 {
     public record Level(int Value)
     {
-        public const int Minimum = 0;
+        public const int Minimum = 1;
         public const int Maximum = 100;
 
         public int Value { get; } = Value >= Minimum && Value <= Maximum ? Value : throw new ArgumentException($"Level must be in range {Minimum}-{Maximum}, got {Value}", nameof(Value));
